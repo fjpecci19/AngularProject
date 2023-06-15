488ca5e790632625848d1d3717c98b4771b6d48e
Franco Pecci <fapeccisan@gmail.com>
2023-06-15 09:08:18 -0400

    a

---
+++ src/app/Formulario/formulario.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core'
+import { NgForm } from '@angular/forms'
+
+@Component({
+    selector: 'app-servers',
+    templateUrl: './formulario.html',
+    styleUrls: ["./formulario.css"]
+})
+
+export class Formulario implements OnInit{
+    
+    formData = {
+        title: "",
+        ult: "",
+        des: "",
+        contacto: "",
+        ciudad: "",
+        pais: "",
+    }
+    
+    reporte = ""
+
+    ngOnInit(){
+        
+    }
+
+    onUpdatereporte(event: any){
+        this.reporte = event.target.value
+    }
+
+    submitForm(form: NgForm){
+        if (form.valid){
+            console.log("Formulario válido")
+        }else{
+            console.log("Formulario inválido")
+        }
+    }
+}
